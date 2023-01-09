ddf493ebd1e90bfb320e63e1beb6b49e39d905d4
Shashank P <shashankp2832@gmail.com>
2023-01-09 15:11:20 +0530

    Added lab 2 part 1

---
+++ 200010048_lab2_part1/hello_world.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+    int i = 0;
+    char hello[] = "Hello World";
+    while(hello[i] != '\0'){
+        int child_pid = fork();
+        if(child_pid == 0) printf("character: %c, PID: %d\n", hello[i++], getpid());
+        else{
+            sleep(5);
+            break;
+        }
+    }
+    return 0;
+}
