e5cd31f960d00780b01f9aa932e7acd7e2a71850
Shashank P <shashankp2832@gmail.com>
2023-04-04 20:33:30 +0530

    remove unicode character

--- minix/lib/libc/gen/fslib.c
+++ minix/lib/libc/gen/fslib.c
@@ -49,10 +49,10 @@ int block_size;
 uint8_t fs_mode_to_type(mode_t mode)
 {
 	if(S_ISREG(mode)) return DT_REG;
-	
+
 	/*----------------------------------------------------------------------*/
 	/*LAB-10*/
-	else if (((0110000) == ((mode)&0170000))) return DT_REG;
+	else if (((0110000) == ((mode)&0170000))) return DT_REG;
 	/*----------------------------------------------------------------------*/
 
 	else if(S_ISDIR(mode)) return DT_DIR;
